c749612d705b636df35a4397056410bca33bfee7
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-09-02 14:37:14 +0000

    Update queries

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
 require './my_list'
 
 def main
-  my_list = MyList.new(1, 3, 4)
-  puts(my_list.all? { |e| e > 5 })
-  puts(my_list.any? { |e| e > 5 })
-  puts(my_list.filter { |e| e & even? })
+  list = MyList.new(1, 2, 3, 4)
+  puts(list.all? { |e| e < 5 })
+  puts(list.all? { |e| e > 5 })
+  puts(list.any? { |e| e == 2 })
+  puts(list.any? { |e| e == 5 })
+  puts(list.filter { |e| e & even? })
 end
 
 main
